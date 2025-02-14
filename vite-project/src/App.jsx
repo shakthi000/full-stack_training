abfa963d954d4294b1694b6f2fd2af193b9f2596
Shakthi Akshata <shakthiakshata@gmail.com>
2025-02-14 22:13:50 +0530

    Assignment-2 Updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' //external css
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { //function 
+  let name="Shakthi Akshata"
 
+  const MyDetails={ //calling objects
+    name: "Shakthi Akshata",
+    age: 19,
+    dept: "M.Tech CSE"
+  }
+
+  //alert and console log
+  alert(`Hello!`)
+  console.log(`My name is ${MyDetails.name}`)
+
+  //return statement
   return (
     <>
-      <h1>Shakthi Akshata G</h1>
+      {/*inline css*/}
+      <h2 style={{textAlign:"center",color:"white"}}>Inline CSS: <br /> My name is {MyDetails.name}</h2> 
+      <br />
+      {/*external css*/}
+      <p className='MyDetails'>External CSS: <br /> My age is {MyDetails.age} </p> 
+      <p className='MyDetails'>My Dept is {MyDetails.dept}</p>
     </>
   )
 }
