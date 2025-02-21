5a45aa1c06420b569fd0c1c605d8a454deb26709
Shakthi Akshata <shakthiakshata@gmail.com>
2025-02-21 22:03:01 +0530

    assignment 6 updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,56 +1,33 @@
 import React, { useState } from "react";
 import "./App.css";
-import Comp1 from "./Comp1";
 
 function App() {
-  const myInfo = {
-    name: "Shakthi Akshata",
-    age: 19,
-    secId: "Sec23cj033",
-    department: "M.Tech CSE"
-  };
+  const[name,setName]=useState("");
+  const[age,setAge]=useState("");
+  const[dept,setDept]=useState("");
+  const SubmitInfo=(e)=>{
+    e.preventDefault();
+    console.log(name);
+    console.log(age);
+    console.log(dept);
+  }
 
-  const herInfo = {
-    name: "Rajalakshmi",
-    age: 19,
-    secId: "Sec23cj001",
-    department: "M.Tech CSE"
-  };
-
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
 
   return (
-    <div className={isDarkMode ? "dark-theme" : "light-theme"}>
-      <div className="content">
-        <h1 style={{ textAlign: "center", color: isDarkMode ? "#F9F6E6" : "darkred" }}>
-          Friends Forever :)
-        </h1>
+    <div className="reg-form">
+      <form className="form" onSubmit={(SubmitInfo)}>
+        <h2>Registration Form</h2>
+        <input className="name" type="text" required value={name} placeholder="Enter Name" onChange={(e)=>setName(e.target.value)} />
+        <input className="age"type="text" required value={age} placeholder="Enter Age" onChange={(e)=>setAge(e.target.value)} />
+        <input className="dept" type="text" required value={dept} placeholder="Enter Department" onChange={(e)=>setDept(e.target.value)} />
+        
+        <input className="email" type="email" required placeholder="Enter Email" />
+        <input className="password" type="password" required placeholder="Enter Password" />
+        <input className="confirm-password" type="password" required placeholder="Confirm Password" />
 
-        <br />
-
-        <div>
-          <h1 style={{ fontSize: "20px", textAlign: "center", color: "#b59199", fontStyle: "italic" }}>
-            Me ♥
-          </h1>
-          <Comp1 name={myInfo.name} age={myInfo.age} secId={myInfo.secId} department={myInfo.department} />
-
-          <h1 style={{ fontSize: "20px", textAlign: "center", color: "#b59199", fontStyle: "italic" }}>
-            Her ♥
-          </h1>
-          <Comp1 name={herInfo.name} age={herInfo.age} secId={herInfo.secId} department={herInfo.department} />
-        </div>
-
-        <button className="action-button" onClick={() => alert("Bye!")}>Click Me!</button>
-        <button className="action-button" onDoubleClick={() => alert("See you later!")}>Click Me!</button>
-
-        <button className="toggle-button" onClick={toggleTheme}>
-          {isDarkMode ? "Light Mode" : "Dark Mode"}
-        </button>
-      </div>
+        <button className="submit" type="submit">Submit</button>
+        <button className="reset">Reset</button>
+      </form>
     </div>
   );
 }
